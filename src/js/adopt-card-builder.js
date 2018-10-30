781fd4a379c9dde33c2abafdd1a313938f2dec3b
ezavile <ezavile@gmail.com>
2018-10-30 00:11:18 -0600

    feat: handle pets as an array and generate card
    - Data Types
    - Function Statement
    - Function Expression
    - Associativity
    - Coercion

--- src/js/adopt-card-builder.js
+++ src/js/adopt-card-builder.js
@@ -1,34 +1,9 @@
-console.log('--- Global Execution Context ---');
-console.log(version);
-console.log('--- Creation and Hoisting ---');
-console.log(name);
-console.log(buildCard);
-// console.log(otherVar); throw an error
-
-var name = 'Benito';
-var details = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.';
-
-var benitoTemplate = buildCard(name, details);
-
-console.log(benitoTemplate);
-
-function buildCard(name, details) {
-  console.log('--- Execution Context ---');
+function buildCard(id, name, details) {
   var name = name.toLowerCase();
 
-  return (
-    '<article class="yta_Adopt-card">' +
-      buildImage() +
-      '<div class="yta_Adopt-card-description">' +
-        '<h3 class="yta_Adopt-card-description-name">' + name + '</h3>' +
-        '<p class="yta_Adopt-card-description-details">' + details + '</p>' +
-        buildLink() +
-      '</div>' +
-    '</article>'
-  );
-  
-  function buildLink() {
-    console.log('--- Lexical Environments And Scope Chain ---');
+  // console.log(buildLink()); throw an error since it is undefined for the hoisting phase
+
+  var buildLink = function() {
     var link = '/adoptar/' + name;
 
     return (
@@ -37,10 +12,23 @@ function buildCard(name, details) {
       '</p>'
     );
   }
+
+
+  var buildImage = function () {
+    return '<div class="yta_Adopt-card-img" style="background-image: url(./images/' + name + '.png)"></div>';
+  }
+
+  return (
+    '<article class="yta_Adopt-card" id="' + id + '">' +
+      buildImage() +
+      '<div class="yta_Adopt-card-description">' +
+        '<h3 class="yta_Adopt-card-description-name">' + name + '</h3>' +
+        '<p class="yta_Adopt-card-description-details">' + details + '</p>' +
+        buildLink() +
+      '</div>' +
+    '</article>'
+  );
 }
 
-function buildImage() {
-  return '<div class="yta_Adopt-card-img" style="background-image: url(./images/' + name + '.png)"></div>';
-}
 
 window.buildCard = buildCard;
