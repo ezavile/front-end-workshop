5094dad24a30f6a151b7dcf656f3e8dc151f865f
ezavile <ezavile@gmail.com>
2018-10-31 22:08:42 -0600

    feat: add pet dynamically
    - callback
    - event listener

--- src/js/pet-card.js
+++ src/js/pet-card.js
@@ -1,4 +1,16 @@
 (function() {
-  var benito = new AdoptCardBuilder(pets[0]);
-  console.log(benito.getBody());
+  var addPet = document.getElementById('addPet');
+  var adoptCards = document.getElementById('adoptCards');
+  
+  var adoptCardsContent;
+  var pet;
+  var i = 0;
+
+  addPet.addEventListener('click', function(ev) {
+    ev.preventDefault();
+    pet = new AdoptCardBuilder(pets[i]);
+    adoptCardsContent = adoptCards.innerHTML + pet.getBody();
+    adoptCards.innerHTML = adoptCardsContent;
+    i++;
+  });
 })();
